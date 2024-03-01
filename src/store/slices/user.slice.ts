a1962dcfdf5501ceeb3c312e8183217c8e951893
Sirius Deis <sirius_deis@f.society>
2024-03-01 23:38:32 +0100

    add a reducer for successful sign in to a user slice

--- src/store/slices/user.slice.ts
+++ src/store/slices/user.slice.ts
@@ -1,47 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface user {}
+interface IUser {}
 
-interface userState {
-  user: user | null;
+interface IUserState {
+  user: IUser | null;
   isLoading: boolean;
   error: Error | null;
   token: string | null;
 }
 
-const startLoading = ({ value }: { value: userState }) => {
-  value.isLoading = true;
-  value.error = null;
+const startLoading = (state: IUserState) => {
+  state.isLoading = true;
+  state.error = null;
 };
 
-const actionFailure = (
-  { value }: { value: userState },
-  action: { payload: Error }
-) => {
-  value.isLoading = false;
-  value.error = action.payload;
+const actionFailure = (state: IUserState, { payload }: { payload: Error }) => {
+  state.isLoading = false;
+  state.error = payload;
+};
+
+const initialState: IUserState = {
+  user: null,
+  isLoading: false,
+  error: null,
+  token: null,
 };
 
 const userSlice = createSlice({
   name: "user",
-  initialState: {
-    value: {
-      user: null,
-      isLoading: false,
-      error: null,
-      token: null,
-    },
-  },
+  initialState,
   reducers: {
     signUpStart: startLoading,
     signInStart: startLoading,
     signOutStart: startLoading,
     signUpSuccess(state) {
-      state.value.isLoading = false;
-      state.value.error = null;
-      state.value.token = null;
+      state.isLoading = false;
+      state.error = null;
+      state.token = null;
     },
-    signInSuccess(state) {},
+    signInSuccess(
+      state,
+      { payload }: { payload: { data: { user: IUser }; token: string } }
+    ) {
+      state.isLoading = false;
+      state.error = null;
+      state.user = payload.data.user;
+      state.token = payload.token;
+    },
     signOutSuccess(state) {},
     signUpFailure: actionFailure,
     signInFailure: actionFailure,

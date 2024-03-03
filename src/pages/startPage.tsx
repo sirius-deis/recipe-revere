6cabda495945c02ff4c11a26c0cdadbc1ba00c09
Sirius Deis <falebsy@gmail.com>
2024-03-03 23:16:03 +0100

    add an image, a paragraph and a button to startPage

--- src/pages/startPage.tsx
+++ src/pages/startPage.tsx
@@ -1,8 +1,22 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import StarterImg from "../assets/starterImg.png";
 import Button from "../components/button/Button";
 
 const StartPage: FC = () => {
-  return <></>;
+  return (
+    <div>
+      <img src={StarterImg} alt="Image of a person that enjoy cooking" />
+      <h1>CookUp</h1>
+      <p>
+        3000+ easy and delicious recipes from the best chefs from around the
+        world
+      </p>
+      <Button>
+        <Link to="/login">Get Started</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default StartPage;

bd2ad9bc54f9f0261f28cac7a09963392eac7ddf
Sirius Deis <falebsy@gmail.com>
2024-03-07 12:35:57 +0100

    add test for CheckboxWithLabel component to check if it matches the snapshot and to check if it is rendered with the right label text

--- src/components/checkbox/CheckboxWithLabel.test.tsx
+++ src/components/checkbox/CheckboxWithLabel.test.tsx
@@ -1,1 +1,25 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import CheckboxWithLabel from "./CheckboxWithLabel";
+
+describe("CheckboxWithLabel component", () => {
+  it("should match snapshot", () => {
+    const { container } = render(
+      <CheckboxWithLabel
+        isChecked={false}
+        labelText="Remember me"
+        onCheckHandler={() => {}}
+      />
+    );
+    expect(container).toMatchSnapshot();
+  });
+  it("should render component with proper text", () => {
+    render(
+      <CheckboxWithLabel
+        isChecked={false}
+        labelText="Remember me"
+        onCheckHandler={() => {}}
+      />
+    );
+    expect(screen.getByText("Remember me")).toBeInTheDocument();
+  });
+});

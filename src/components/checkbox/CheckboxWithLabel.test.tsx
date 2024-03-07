204d82eb9af4cc262bab119e1233033b0392cec1
Sirius Deis <falebsy@gmail.com>
2024-03-07 12:56:03 +0100

    add a test for checking if event on a checkbox is fired properly

--- src/components/checkbox/CheckboxWithLabel.test.tsx
+++ src/components/checkbox/CheckboxWithLabel.test.tsx
@@ -22,4 +22,21 @@ describe("CheckboxWithLabel component", () => {
     );
     expect(screen.getByText("Remember me")).toBeInTheDocument();
   });
+  it("should call the function when a change event is fired", () => {
+    const onCheckHandler = jest.fn();
+    render(
+      <CheckboxWithLabel
+        isChecked={false}
+        labelText="Remember me"
+        onCheckHandler={onCheckHandler}
+      />
+    );
+    const checkboxLabel = screen.getByLabelText("Remember me");
+    fireEvent.click(checkboxLabel);
+    expect(onCheckHandler).toHaveBeenCalled();
+    expect(onCheckHandler).toHaveBeenCalledTimes(1);
+    const checkbox = checkboxLabel?.nextElementSibling;
+    fireEvent.click(checkbox!);
+    expect(onCheckHandler).toHaveBeenCalledTimes(2);
+  });
 });

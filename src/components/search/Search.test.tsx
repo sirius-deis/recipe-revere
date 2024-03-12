6471f9005cd72db888c420673a7d342f4217e249
Sirius Deis <falebsy@gmail.com>
2024-03-12 23:35:23 +0100

    add a test for checking if component render its placeholder correctly

--- src/components/search/Search.test.tsx
+++ src/components/search/Search.test.tsx
@@ -6,4 +6,8 @@ describe("Search component", () => {
     const { container } = render(<Search placeholderText="Ex.: Something" />);
     expect(container).toMatchSnapshot();
   });
+  it("should render component with the correct placeholder text", () => {
+    render(<Search placeholderText="Ex.: Something" />);
+    expect(screen.getByPlaceholderText("Ex.: Something")).toBeInTheDocument();
+  });
 });

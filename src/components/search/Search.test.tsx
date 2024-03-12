f351a7668b505787d896816c1650542920aabc0b
Sirius Deis <falebsy@gmail.com>
2024-03-12 23:32:23 +0100

    add a test to check if component matches snapshot

--- src/components/search/Search.test.tsx
+++ src/components/search/Search.test.tsx
@@ -1,1 +1,9 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import Search from "./Search";
+
+describe("Search component", () => {
+  it("should match snapchat", () => {
+    const { container } = render(<Search placeholderText="Ex.: Something" />);
+    expect(container).toMatchSnapshot();
+  });
+});

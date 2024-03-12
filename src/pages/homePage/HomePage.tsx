a04ed99bb972dba7496ca0796221cd5ce805c737
Sirius Deis <falebsy@gmail.com>
2024-03-12 23:28:55 +0100

    add styles for search component

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react";
 import styles from "./HomePage.module.css";
+import Search from "../../components/search/Search";
 
 const HomePage: FC = () => {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.home}>
+      <h2>What are we cooking today?</h2>
+      <Search placeholderText="Ex.: Burger" />
+    </div>
+  );
 };
 
 export default HomePage;

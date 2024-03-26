b2157308c0e03256b881f55e8b3887ff5538ad23
Sirius Deis <falebsy@gmail.com>
2024-03-26 22:18:55 +0100

    update button styles, add a new color for action button. add opportunity to add inline styles to a button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,18 +4,21 @@ import styles from "./Button.module.css";
 interface ButtonProps {
   size?: "lg" | "sm";
   bg?: "main" | "action" | "icon";
+  inlineStyles?: {};
 }
 
 const Button: FC<PropsWithChildren & ButtonProps> = ({
   children,
   size,
   bg,
+  inlineStyles,
 }): JSX.Element => {
   return (
     <button
       className={`${styles.btn} ${
         size ? `${styles[`btn-${size}`]}` : "btn-lg"
       } ${bg ? `${styles[`btn-${bg}`]}` : "btn-main"}`}
+      style={inlineStyles}
     >
       {children}
     </button>

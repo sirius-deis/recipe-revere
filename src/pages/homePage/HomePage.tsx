df6b32a71a8f47a95b539c61c267a00ecb77d554
Sirius Deis <falebsy@gmail.com>
2024-03-28 22:51:33 +0100

    add a link to a search page to a section which represents recipes by a choosen category

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { IoOptionsSharp } from "react-icons/io5";
 import styles from "./HomePage.module.css";
 import Search from "../../components/search/Search";
 import Button from "../../components/button/Button";
 import RecipeOfTheDay from "../../components/recipeOfTheDay/RecipeOfTheDay";
+import Row from "../../components/row/Row";
 
 const HomePage: FC = () => {
   return (
     <div className={styles.home}>
-      <h2 className={styles.heading}>What are we cooking today?</h2>
+      <h1 className={styles.heading}>What are we cooking today?</h1>
       <div className={styles["search-with-filter"]}>
         <Search placeholderText="Ex.: Burger" />
         <Button bg="icon">
@@ -16,13 +18,19 @@ const HomePage: FC = () => {
         </Button>
       </div>
       <section className="d-flex" style={{ gap: "2rem", marginTop: "1rem" }}>
-        <h3 className={styles.title}>Recipe of the day</h3>
+        <h2 className={styles.title}>Recipe of the day</h2>
         <RecipeOfTheDay
           ratingAvg={4.9}
           title="Creamy Funghi Risotto"
           image="https://www.inspiredtaste.net/wp-content/uploads/2011/12/Mushroom-Risotto-Recipe-1-1200.jpg"
           tags={["Healthy", "Vegetarian", "Lunch"]}
         />
+      </section>
+      <section className="mt-1">
+        <Row inlineStyles={{ justifyContent: "space-between" }}>
+          <h2 className={styles.title}>Categories</h2>
+          <Link to="/search">See all</Link>
+        </Row>
       </section>
     </div>
   );

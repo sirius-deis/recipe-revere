3c16df415643341a99e2a1b505fb63ea8b0fe1fe
Sirius Deis <falebsy@gmail.com>
2024-04-07 23:37:36 +0200

    add a hook for fetching users

---
+++ src/store/queries.tsx
@@ -0,0 +1,47 @@
+import { gql, useQuery } from "@apollo/client";
+import Loader from "../components/loader/Loader";
+import { FC } from "react";
+
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+  pictures: string[];
+}
+
+const getUsers = gql`
+  query getUsers {
+    users {
+      _id
+      name
+      email
+      role
+      pictures
+    }
+    amount
+  }
+`;
+
+const UserList: FC = () => {
+  const { loading, error, data } = useQuery(getUsers);
+  if (loading) {
+    return <Loader />;
+  }
+  if (error) {
+  }
+  return (
+    <ul>
+      {data.users.map((user: User) => {
+        return (
+          <li key={user._id}>
+            <img src={user.pictures[0]} alt="user" />
+            <h2>user.name</h2>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default UserList;

ae8c7da43b83cf24c341449354c0311b77938681
Sirius Deis <falebsy@gmail.com>
2024-04-08 23:26:35 +0200

    add a component for register mutation

--- src/queries/register.tsx
+++ src/queries/register.tsx
@@ -1,4 +1,7 @@
 import { gql, useMutation } from "@apollo/client";
+import { FC, useState } from "react";
+import InputWithLabel from "../components/input/InputWithLabel";
+import Button from "../components/button/Button";
 
 const REGISTER = gql`
   mutation Register($email: String!, $password: String!. passwordConfirm: String!) {
@@ -9,3 +12,46 @@ const REGISTER = gql`
     }
   }
 `;
+
+const Register: FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [register] = useMutation(REGISTER);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await register({
+      variables: {
+        email,
+        password,
+        passwordConfirm,
+      },
+    });
+  };
+
+  return (
+    <form>
+      <InputWithLabel
+        labelText="Email"
+        inputPlaceholder="example@email.com"
+        icon="email"
+      />
+      <InputWithLabel
+        labelText="Password"
+        inputPlaceholder="************"
+        type="password"
+        icon="password"
+      />
+      <InputWithLabel
+        labelText="Password Confirm"
+        inputPlaceholder="************"
+        type="password"
+        icon="password"
+      />
+      <Button>Sign Up</Button>
+    </form>
+  );
+};
+
+export default Register;

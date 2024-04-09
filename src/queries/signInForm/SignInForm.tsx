9cd35340248583b25b8cf0cf0d9a7884658c497f
Sirius Deis <falebsy@gmail.com>
2024-04-09 23:23:08 +0200

    add a html for a signInForm component and bind submit event to a form

--- src/queries/signInForm/SignInForm.tsx
+++ src/queries/signInForm/SignInForm.tsx
@@ -1,4 +1,10 @@
 import { gql, useMutation } from "@apollo/client";
+import { FC, useState } from "react";
+import InputWithLabel from "../../components/input/InputWithLabel";
+import Button from "../../components/button/Button";
+import Row from "../../components/row/Row";
+import CheckboxWithLabel from "../../components/checkbox/CheckboxWithLabel";
+import { Link } from "react-router-dom";
 
 const LOGIN = gql`
   mutation Login($email: String!, $password: String!) {
@@ -8,3 +14,57 @@ const LOGIN = gql`
     }
   }
 `;
+
+const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+  const [login] = useMutation(LOGIN);
+
+  const onCheckHandler = () => {
+    setIsChecked((prevState) => !prevState);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await login({
+      variables: {
+        email,
+        password,
+      },
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <InputWithLabel
+        labelText="Email"
+        inputPlaceholder="example@email.com"
+        icon="email"
+      />
+      <InputWithLabel
+        labelText="Password"
+        inputPlaceholder="************"
+        type="password"
+        icon="password"
+      />
+
+      <Row inlineStyles={{ gap: "2rem" }}>
+        <CheckboxWithLabel
+          isChecked={isChecked}
+          labelText="Remember me"
+          onCheckHandler={onCheckHandler}
+        />
+        <Link
+          to="/forgot"
+          style={{ fontSize: "1.5rem", textDecoration: "underline" }}
+        >
+          Forgot password?
+        </Link>
+      </Row>
+      <Button>Sign In</Button>
+    </form>
+  );
+};
+
+export default SignInForm;

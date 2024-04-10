e48f46da3be4e825edc75ed34f817e5f51c4f28e
Sirius Deis <falebsy@gmail.com>
2024-04-10 23:16:00 +0200

    add html to forgetPasswordForm component

--- src/queries/forgetPasswordForm/ForgetPassword.tsx
+++ src/queries/forgetPasswordForm/ForgetPassword.tsx
@@ -1,4 +1,7 @@
 import { gql, useMutation } from "@apollo/client";
+import { FC, useState } from "react";
+import InputWithLabel from "../../components/input/InputWithLabel";
+import Button from "../../components/button/Button";
 
 const FORGET_PASSWORD = gql`
   mutation ForgetPassword($userEmail: String!) {
@@ -7,3 +10,22 @@ const FORGET_PASSWORD = gql`
     }
   }
 `;
+
+const ForgetPasswordForm: FC = () => {
+  const [userEmail, setUserEmail] = useState("");
+  const [forgetPassword, { data, loading, error }] =
+    useMutation(FORGET_PASSWORD);
+
+  return (
+    <form>
+      <InputWithLabel
+        labelText="Email"
+        inputPlaceholder="example@email.com"
+        icon="email"
+      />
+      <Button>Submit</Button>
+    </form>
+  );
+};
+
+export default ForgetPasswordForm;

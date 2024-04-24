f632e74ae3eb365c2b00d003f55830da0379f53f
Sirius Deis <falebsy@gmail.com>
2024-04-24 23:35:20 +0200

    map fetched recipes to return a formed savedRecipe components

--- src/components/savedRecipeList/SavedRecipeList.tsx
+++ src/components/savedRecipeList/SavedRecipeList.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { gql, useQuery } from "@apollo/client";
 import Loader from "../loader/Loader";
+import SavedRecipe from "../savedRecipe/SavedRecipe";
 
 const GET_SAVED_RECIPES = gql`
   query getSavedRecipes {
@@ -14,7 +15,25 @@ const GET_SAVED_RECIPES = gql`
 
 const SavedRecipeList: FC = () => {
   const { loading, error, data } = useQuery(GET_SAVED_RECIPES);
-  return <section></section>;
+  if (loading) {
+    return <Loader />;
+  }
+  if (error) {
+  }
+  return (
+    <section>
+      {data.recipes.map(
+        (recipe: { url: string; label: string; image: string }) => (
+          <SavedRecipe
+            key={recipe.url}
+            imgUrl={recipe.image}
+            title={recipe.label}
+            url={recipe.url}
+          />
+        )
+      )}
+    </section>
+  );
 };
 
 export default SavedRecipeList;

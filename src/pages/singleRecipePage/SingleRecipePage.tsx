7d40721deab60f22a874ddc9f0578475be2e0ab4
Sirius Deis <falebsy@gmail.com>
2024-05-01 23:46:50 +0200

    add a query definition for getting recipe

--- src/pages/singleRecipePage/SingleRecipePage.tsx
+++ src/pages/singleRecipePage/SingleRecipePage.tsx
@@ -1,5 +1,37 @@
 import { FC } from "react";
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+
+const GET_RECIPE = gql`
+  query getRecipe($id: String) {
+    recipe(id: $id) {
+      recipe {
+        url
+        label
+        image
+        source
+        dietLabels
+        healthLabels
+        cautions
+        ingredientLines
+        calories
+        totalWeight
+        totalTime
+        cuisineType
+        mealType
+        dishType
+      }
+      reviews {
+        _id
+        userId
+        review
+        rating
+      }
+      averageRating
+      amountOfReviews
+    }
+  }
+`;
 
 const SingleRecipePage: FC = () => {
   const { recipeId } = useParams();

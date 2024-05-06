31d1cda85f2bc48bdae2188201e550be25426e1a
Sirius Deis <falebsy@gmail.com>
2024-05-06 23:53:49 +0200

    add an interface for ingredient

---
+++ src/components/ingredientsList/IngredientsList.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+interface Ingredient {
+    quantity: number,
+    measure: string,
+    food: string,
+    weight: number,
+    foodId: string,
+    image: string
+}
+
+const IngredientsList: FC = ({}) => {
+    return <ul>
+
+    </ul>
+}
+
+export default IngredientsList;

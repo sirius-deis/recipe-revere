768edc97adddad9d6fc6fa4928f2307e54711de0
Sirius Deis <falebsy@gmail.com>
2024-05-08 23:58:22 +0200

    add styles to accordion component

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,17 +1,20 @@
 import { FC, PropsWithChildren } from "react";
-import styles from './Accordion.module.css'
+import styles from "./Accordion.module.css";
 
 interface AccordionProps {
-    title: string;
+  title: string;
 }
 
-const Accordion: FC<PropsWithChildren & AccordionProps> = ({children, title}) => {
-    return <section>
-        <button className={styles.accordion}>{title}</button>
-        <div className={styles.panel}>
-            {children}
-        </div>
+const Accordion: FC<PropsWithChildren & AccordionProps> = ({
+  children,
+  title,
+}) => {
+  return (
+    <section>
+      <button className={styles.accordion}>{title}</button>
+      <div className={styles.panel}>{children}</div>
     </section>
-}
+  );
+};
 
 export default Accordion;

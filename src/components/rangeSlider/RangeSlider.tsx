58b38744481d831e1566f8405d074f37a4365d03
Sirius Deis <falebsy@gmail.com>
2024-05-09 23:54:55 +0200

    update html for rangel slider

--- src/components/rangeSlider/RangeSlider.tsx
+++ src/components/rangeSlider/RangeSlider.tsx
@@ -11,10 +11,26 @@ interface RangeSliderProps {
   withLabel: boolean;
 }
 
-const RangeSlider: FC<RangeSliderProps> = () => {
+const RangeSlider: FC<RangeSliderProps> = ({
+  max,
+  min,
+  value,
+  onMouseDown,
+  onTouchStart,
+  width,
+  withLabel,
+}) => {
   return (
-    <div className={styles["range-slider"]}>
-      <div></div>
+    <div
+      className={styles["range-slider"]}
+      style={{ width: width ? `${width}px` : "100%" }}
+    >
+      <div
+        className={styles["range-slider-bar"]}
+        style={{ width: `${((value - min) / (max - min)) * 100}%` }}
+      >
+        <span className={styles["range-slider-handle"]}></span>
+      </div>
     </div>
   );
 };

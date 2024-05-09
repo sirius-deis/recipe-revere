01a2df8df082327582e2686f0eb2961e1d8e670c
Sirius Deis <falebsy@gmail.com>
2024-05-09 23:46:33 +0200

    replace onChange props event with onMouseDown and onTouchStart

--- src/components/rangeSlider/RangeSlider.tsx
+++ src/components/rangeSlider/RangeSlider.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
+import styles from "./RangleSlider.module.css";
 
 interface RangeSliderProps {
   min: number;
   max: number;
   value: number;
-  onChange: (value: number) => void;
+  onMouseDown: (value: number) => void;
+  onTouchStart: (value: number) => void;
   width: number;
   withLabel: boolean;
 }
 
 const RangeSlider: FC<RangeSliderProps> = () => {
-  return <div></div>;
+  return (
+    <div className={styles["range-slider"]}>
+      <div></div>
+    </div>
+  );
 };
 
 export default RangeSlider;

602225ab2c4386b63bc27492fd01793f7f324803
Sirius Deis <falebsy@gmail.com>
2024-05-09 23:33:36 +0200

    add an empty accordion to a search page

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -1,12 +1,16 @@
 import { FC } from "react";
 import Search from "../../components/search/Search";
+import Accordion from "../../components/accordion/Accordion";
 
 const SearchPage: FC = () => {
-  return <div>
-    <h1>Search</h1>
-    <Search placeholderText="Ex.: Veggie Burger"/>
-    <h2>Filter by</h2>
-  </div>;
+  return (
+    <div>
+      <h1>Search</h1>
+      <Search placeholderText="Ex.: Veggie Burger" />
+      <h2>Filter by</h2>
+      <Accordion title="Categories"></Accordion>
+    </div>
+  );
 };
 
 export default SearchPage;

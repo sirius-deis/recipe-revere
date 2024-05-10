42978efa90cf6885255290d7c085eaec56f0e637
Sirius Deis <falebsy@gmail.com>
2024-05-10 23:57:25 +0200

    fix regexp for an email

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import { Outlet, useNavigate } from "react-router-dom";
+import { getToken } from "../utils/store";
 
 const RootLayout: FC = () => {
   const navigate = useNavigate();

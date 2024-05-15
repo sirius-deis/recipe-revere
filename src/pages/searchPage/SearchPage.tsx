2649fd1ac9f9bc513bab0ceffa3e6f5ba054afe9
Sirius Deis <falebsy@gmail.com>
2024-05-15 23:57:40 +0200

    add a categories array to a cartGroup in the first accordion

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -1,7 +1,16 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
+import { FaLeaf } from "react-icons/fa";
 import Search from "../../components/search/Search";
 import Accordion from "../../components/accordion/Accordion";
 import RangeSlider from "../../components/rangeSlider/RangeSlider";
+import CartGroup from "../../components/cartGroup/CartGroup";
+
+const categories: { title: string; icon: ReactNode }[] = [
+  {
+    title: "Vegetarian",
+    icon: <FaLeaf />,
+  },
+];
 
 const SearchPage: FC = () => {
   return (
@@ -9,7 +18,9 @@ const SearchPage: FC = () => {
       <h1>Search</h1>
       <Search placeholderText="Ex.: Veggie Burger" />
       <h2>Filter by</h2>
-      <Accordion title="Categories"></Accordion>
+      <Accordion title="Categories">
+        <CartGroup carts={categories} />
+      </Accordion>
       <Accordion title="Diet"></Accordion>
       <Accordion title="Meal Type"></Accordion>
       <Accordion title="Calories">

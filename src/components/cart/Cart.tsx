8e9d3379f42e0d7e5ab367699e4a2a4b0d7fed9d
Sirius Deis <falebsy@gmail.com>
2024-05-15 23:42:13 +0200

    add a styles definition to a cart component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from "react";
+import styles from "./Cart.module.css";
 
 interface CartProps {
   title: string;
@@ -7,9 +8,9 @@ interface CartProps {
 
 const Cart: FC<CartProps> = ({ title, icon }) => {
   return (
-    <article>
-      <h3>{title}</h3>
-      {icon}
+    <article className={styles.cart}>
+      <h3 className={styles.title}>{title}</h3>
+      <div className={styles.icon}>{icon}</div>
     </article>
   );
 };

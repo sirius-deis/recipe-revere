e265eaebf4868358f790f9a506538e0515154fdc
Sirius Deis <falebsy@gmail.com>
2024-05-22 23:55:11 +0200

    add styles to a list component and add prop to represent direction of the list

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -2,9 +2,13 @@ import React, { FC, PropsWithChildren, ReactNode } from "react";
 
 import styles from "./List.module.css";
 
-const List: FC<PropsWithChildren> = ({ children }) => {
+interface ListProps {
+  direction: "vertical" | "horizontal";
+}
+
+const List: FC<PropsWithChildren & ListProps> = ({ children, direction }) => {
   return (
-    <ul className={styles.list}>
+    <ul className={`${styles.list} ${styles[direction]}`}>
       {React.Children.map<ReactNode, ReactNode>(children, (child) => {
         return React.cloneElement(child as React.ReactElement, {});
       })}

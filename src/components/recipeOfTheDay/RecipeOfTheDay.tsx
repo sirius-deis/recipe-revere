ca735a29bb43a3d965dad17863c70c627a6cb2f3
Sirius Deis <falebsy@gmail.com>
2024-06-03 22:19:56 +0200

    replace html structure in recipeOfTheDay component with recipePreview component

--- src/components/recipeOfTheDay/RecipeOfTheDay.tsx
+++ src/components/recipeOfTheDay/RecipeOfTheDay.tsx
@@ -1,12 +1,7 @@
 import { FC } from "react";
 import { gql, useQuery } from "@apollo/client";
-import { FaStar, FaHeart, FaRegHeart } from "react-icons/fa";
-import styles from "./RecipeOfTheDay.module.css";
-import Row from "../row/Row";
-import Col from "../column/Col";
-import Button from "../button/Button";
 import Loader from "../loader/Loader";
-import Panel from "../panel/Panel";
+import RecipePreview from "../recipePreview/recipePreview";
 
 const RECIPE_OF_THE_DAY = gql`
   query GetRecipe($tags: [String]) {
@@ -23,7 +18,7 @@ const RECIPE_OF_THE_DAY = gql`
     }
   }
 `;
-//TODO: replace the button with a link
+
 const RecipeOfTheDay: FC = () => {
   const { loading, error, data } = useQuery(RECIPE_OF_THE_DAY, {
     variables: {
@@ -37,39 +32,14 @@ const RecipeOfTheDay: FC = () => {
     //TODO: add error img
   }
   return (
-    <Panel withShadow>
-      <div
-        className={styles["image-container"]}
-        style={{ backgroundImage: `url(${data?.recipe.image})` }}
-      >
-        <div className={styles.rating}>
-          <FaStar />
-          {data?.averageRating}
-        </div>
-        <div className={styles.liked}>
-          <Button bg="icon" size="sm" style={{ padding: "0.8rem" }} rounded>
-            <FaHeart />
-          </Button>
-        </div>
-      </div>
-      <div className={styles.desc}>
-        <Row inlineStyles={{ justifyContent: "space-between" }}>
-          <Col inlineStyles={{ gap: "0.5rem" }}>
-            <h3 className={styles.title}>{data?.recipe.label}</h3>
-            <ul className={styles.tags}>
-              {data?.recipe.dietLabels.map((tag: string) => (
-                <li key={tag}>{tag}</li>
-              ))}
-            </ul>
-          </Col>
-          <Col>
-            <Button bg="action" size="sm">
-              Cook now
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    </Panel>
+    <RecipePreview
+      id={data.recipe.url}
+      label={data?.recipe.label}
+      image={data?.recipe.image}
+      avgRating={data?.averageRating}
+      dietLabels={data?.recipe.dietLabels}
+      btnTitle="Cook now"
+    />
   );
 };
 

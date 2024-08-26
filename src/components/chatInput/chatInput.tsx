f6226c015f40220531424072ab4aedbef441daf4
Sirius Deis <falebsy@gmail.com>
2024-08-26 11:40:57 +0200

    add a state to a chatInput component

--- src/components/chatInput/chatInput.tsx
+++ src/components/chatInput/chatInput.tsx
@@ -1,16 +1,21 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { BsSend } from "react-icons/bs";
 import styles from './chatInput.module.css'
 import InputWithLabel from "../input/InputWithLabel";
 import Button from "../button/Button";
 
 const ChatInput: FC = () => {
+  const [message, setMessage] = useState('');
   return <div className={styles.chatInput}>
     <InputWithLabel
       type="text"
       placeholder="Type a message..."
       isValid={true}
       className="mb-3"
+      value={message}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+        setMessage(e.target.value)
+      }
     />
     <Button type="submit" className="btn btn-primary">
       <BsSend/>

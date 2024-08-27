5066e8cc196111f66ce9bfe47d3f3b5f6113367d
Sirius Deis <falebsy@gmail.com>
2024-08-27 12:37:44 +0200

    add a html for a message component

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -5,11 +5,18 @@ interface MessageProps {
   message: string;
   timestamp: string;
   isRead: boolean;
+  isMine?: boolean;
   onClick?: () => void;
 }
 
-const Message:FC<MessageProps> = () => {
-  return <div></div>
+const Message:FC<MessageProps> = ({sender, message, timestamp, isMine}) => {
+  return <div className="">
+    <div className="">
+      {!isMine && <span className="">(sender)</span>}
+      <span className="">{message}</span>
+      <span className="">{timestamp}</span>
+    </div>
+  </div>
 }
 
 export default Message;

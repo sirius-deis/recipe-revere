ab055b1e43d32cb5c127dfc780632205149f3d8e
Sirius Deis <falebsy@gmail.com>
2024-09-29 19:07:36 +0200

    replace span element for showing a message with textMessage component

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import styles from './Message.module.css'
 import { MessageProps } from "../../types/types";
+import TextMessage from "../textMessage/TextMessage";
 import MessageInfo from "../messageInfo/MessageInfo";
 
 const Message: FC<MessageProps> = ({ message, timestamp, isRead = false }) => {
   return <div className={styles['message-container']}>
-    <span className={styles.message}>{message}</span>
+    <TextMessage message={message} />
     <MessageInfo timestamp={timestamp} isRead={isRead} />
   </div>
 }

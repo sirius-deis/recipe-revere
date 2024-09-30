4574932fdfa1ef78cc7ef244e5778fbc9e2ae07e
Sirius Deis <falebsy@gmail.com>
2024-09-30 19:49:19 +0200

    add a interface in imageMessage component

--- src/components/imageMessage/ImageMessage.tsx
+++ src/components/imageMessage/ImageMessage.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
 
-const ImageMessage: FC<{ src: string, desc: string }> = ({ src, desc }) => {
+interface IImageMessage {
+  src: string;
+  desc: string;
+  [x: string]: any;
+}
+
+const ImageMessage: FC<IImageMessage> = ({ src, desc }) => {
   return (
     <div>
       <img src={src} alt={desc} />

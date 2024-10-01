fee533ed5f374a05ef51872877c8050e925e6818
Sirius Deis <falebsy@gmail.com>
2024-10-01 19:33:33 +0200

    wrap a message component in a panel component

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -4,12 +4,15 @@ import { MessageProps, types } from "../../types/types";
 import TextMessage from "../textMessage/TextMessage";
 import MessageInfo from "../messageInfo/MessageInfo";
 import ImageMessage from "../imageMessage/ImageMessage";
+import Panel from "../panel/Panel";
 
 const Message: FC<MessageProps> = ({ message, timestamp, isRead = false, type, src }) => {
   return <div className={styles['message-container']}>
-    <TextMessage message={message} />
-    <MessageInfo timestamp={timestamp} isRead={isRead} />
-    {type === types.image && <ImageMessage src={src} />}
+    <Panel withBorder>
+      <TextMessage message={message} />
+      <MessageInfo timestamp={timestamp} isRead={isRead} />
+      {type === types.image && <ImageMessage src={src} />}
+    </Panel>
   </div>
 }
 

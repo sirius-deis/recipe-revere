0a026bab23527d4a36ca3448041d99529f258eb5
Sirius Deis <falebsy@gmail.com>
2024-10-01 19:30:28 +0200

    add rendering imageMessage component to a message component if the type is an image

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -3,11 +3,13 @@ import styles from './Message.module.css'
 import { MessageProps, types } from "../../types/types";
 import TextMessage from "../textMessage/TextMessage";
 import MessageInfo from "../messageInfo/MessageInfo";
+import ImageMessage from "../imageMessage/ImageMessage";
 
 const Message: FC<MessageProps> = ({ message, timestamp, isRead = false, type, src }) => {
   return <div className={styles['message-container']}>
     <TextMessage message={message} />
     <MessageInfo timestamp={timestamp} isRead={isRead} />
+    {type === types.image && <ImageMessage src={src} />}
   </div>
 }
 

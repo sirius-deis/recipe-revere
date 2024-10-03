53895a7702ce16e8495220f417db91107df380d1
Sirius Deis <falebsy@gmail.com>
2024-10-03 20:26:35 +0200

    add a function to play and pause an audio

--- src/components/audioMessage/audioMessage.tsx
+++ src/components/audioMessage/audioMessage.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useRef, useState } from "react";
 import { FaPlay } from "react-icons/fa6";
 import { FaPause } from "react-icons/fa6";
 import Button from "../button/Button";
@@ -10,10 +10,21 @@ interface IAudioMessage {
 
 const AudioMessage: FC<IAudioMessage> = ({ src }) => {
   const [isListening, setIsListening] = useState(false);
+  const ref = useRef<HTMLAudioElement>(null);
+
+  const toggleAudio = () => {
+    setIsListening((prevState) => !prevState);
+    if (isListening) {
+      ref.current?.play();
+    }
+    else {
+      ref.current?.pause();
+    }
+  };
 
   return <div>
-    <audio src={src}></audio>
-    <Button size="sm" rounded onClick={() => setIsListening(!isListening)}>
+    <audio src={src} ref={ref}></audio>
+    <Button size="sm" rounded onClick={toggleAudio}>
       {isListening ? <FaPlay /> : <FaPause />}
     </Button>
   </div>

cc70b88192828044c3e60b046cacf6ed7702b90d
Sirius Deis <falebsy@gmail.com>
2024-10-09 23:19:56 +0200

    add audioVisualizer to an audioMessage component

--- src/components/audioMessage/audioMessage.tsx
+++ src/components/audioMessage/audioMessage.tsx
@@ -1,6 +1,7 @@
 import { FC, useRef, useState } from "react";
 import { FaPlay } from "react-icons/fa6";
 import { FaPause } from "react-icons/fa6";
+import { AudioVisualizer } from 'react-audio-visualize';
 import Button from "../button/Button";
 
 interface IAudioMessage {
@@ -11,6 +12,8 @@ interface IAudioMessage {
 const AudioMessage: FC<IAudioMessage> = ({ src }) => {
   const [isListening, setIsListening] = useState(false);
   const ref = useRef<HTMLAudioElement>(null);
+  const visualizerRef = useRef<HTMLCanvasElement>(null);
+
 
   const toggleAudio = () => {
     setIsListening((prevState) => !prevState);
@@ -27,6 +30,9 @@ const AudioMessage: FC<IAudioMessage> = ({ src }) => {
     <Button size="sm" rounded onClick={toggleAudio}>
       {isListening ? <FaPlay /> : <FaPause />}
     </Button>
+    {
+      src && <AudioVisualizer ref={visualizerRef} blob={null} width={500} height={75} barWidth={1} gap={0} />
+    }
   </div>
 }
 

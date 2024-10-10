65f7d4816c93741f86652259c39c1e4b15e8a474
Sirius Deis <falebsy@gmail.com>
2024-10-10 23:11:47 +0200

    add a swith statemant to return either json or blob from response in fetchData function

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,6 +1,6 @@
 const baseUrl = 'http://localhost:3000/api/v1/'
 
-const fetchData = async (url: string, options: any) => {
+const fetchData = async (url: string, options: any, type: "json" | "blob"): Promise<any> => {
   const response = await fetch(`${baseUrl}${url}`, {
     credentials: 'include',
     method: 'GET',
@@ -17,7 +17,16 @@ const fetchData = async (url: string, options: any) => {
   let data;
   if(response.body) {
     try {
-      data = await response.json();
+      switch (type) {
+        case "json":
+          data = await response.json();
+          break;
+        case "blob":
+          data = await response.blob();
+          break;
+        default:
+          data = await response.text()
+      }
     } catch(err) {
       return;
     }

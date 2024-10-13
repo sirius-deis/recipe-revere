1c6beeb6476199e7f634f0f8c4abcaa50ba63bfe
Sirius Deis <falebsy@gmail.com>
2024-10-13 21:22:38 +0200

    add AudioMessage to a Message component

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -5,6 +5,7 @@ import TextMessage from "../textMessage/TextMessage";
 import MessageInfo from "../messageInfo/MessageInfo";
 import ImageMessage from "../imageMessage/ImageMessage";
 import Panel from "../panel/Panel";
+import AudioMessage from "../audioMessage/audioMessage";
 
 const Message: FC<MessageProps> = ({ message, timestamp, isRead = false, type, src }) => {
   return <div className={styles['message-container']}>
@@ -12,6 +13,7 @@ const Message: FC<MessageProps> = ({ message, timestamp, isRead = false, type, s
       <TextMessage message={message} />
       <MessageInfo timestamp={timestamp} isRead={isRead} />
       {type === types.image && <ImageMessage src={src} />}
+      {type === types.audio && <AudioMessage src={src} />}
     </Panel>
   </div>
 }
